f86aa460232c296d648290c695192cf112aa42b4
ABrisset <ant.brisset@gmail.com>
2019-01-17 14:42:59 +0100

    Update configuration.rb
    Add x_api_version attribute

--- lib/football__data/configuration.rb
+++ lib/football__data/configuration.rb
@@ -3,22 +3,25 @@ module FootballData
         DEFAULT_API_KEY = nil
         DEFAULT_API_VERSON   = "alpha"
         DEFAULT_RESPONSE_CONTROL = "full"
+        DEFAULT_X_API_VERSION = 'v2.1'
 
         API_ENDPOINT = "http://api.football-data.org"
 
-        attr_writer :api_version, :api_key, :response_control
+        attr_writer :api_version, :api_key, :response_control, :x_api_version
 
         def configure
             raise "don't you wanna configure it?" unless block_given?
             yield self
             @api_version ||= DEFAULT_API_VERSON
             @response_control ||= DEFAULT_RESPONSE_CONTROL
+            @x_api_version = DEFAULT_X_API_VERSON
         end
 
         def reset!
             @api_key = DEFAULT_API_KEY
             @api_version = DEFAULT_API_VERSON
             @response_control = DEFAULT_RESPONSE_CONTROL
+            @x_api_version = DEFAULT_X_API_VERSON
         end
     end
 end
