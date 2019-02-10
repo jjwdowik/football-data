2a04c8abc3c86843c4c2be76de0cea965deb8953
jjwdowik <jjwdowik@umich.edu>
2019-02-09 21:43:08 -0500

    remove api version"

--- lib/football__data/configuration.rb
+++ lib/football__data/configuration.rb
@@ -1,7 +1,6 @@
 module FootballData
     module Configuration
         DEFAULT_API_KEY = nil
-        DEFAULT_API_VERSON   = "alpha"
         DEFAULT_RESPONSE_CONTROL = "full"
         DEFAULT_X_API_VERSION = 'v2'
 
@@ -12,14 +11,12 @@ module FootballData
         def configure
             raise "don't you wanna configure it?" unless block_given?
             yield self
-            @api_version ||= DEFAULT_API_VERSON
             @response_control ||= DEFAULT_RESPONSE_CONTROL
             @x_api_version ||= DEFAULT_X_API_VERSION
         end
 
         def reset!
             @api_key = DEFAULT_API_KEY
-            @api_version = DEFAULT_API_VERSON
             @response_control = DEFAULT_RESPONSE_CONTROL
             @x_api_version = DEFAULT_X_API_VERSION
         end
